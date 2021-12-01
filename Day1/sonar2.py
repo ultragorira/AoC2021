b9f55aa497f57a49955d5ce2c2349ed5904e5dcd
ultragorira <lorisdeluca1988@gmail.com>
2021-12-01 12:18:22 +0200

    Prob2

--- Day1/sonar2.py
+++ Day1/sonar2.py
@@ -1,9 +1,14 @@
 with open(r'./Day1/sonar.txt', 'r') as f:
-    DATA = [int(x) for x in f.readlines()]
+    file_cont = [int(x) for x in f.readlines()]
 
 
-def part_one(data=DATA):
+def count_increase(data=file_cont):
     return sum(1 if data[x] > data[x - 1] else 0 for x in range(1, len(data)))
 
+def triple_measure():
+    return count_increase(
+        [file_cont[x] + file_cont[x - 1] + file_cont[x - 2] for x in range(len(file_cont)) if x >= 2]
+    )
 
-print(part_one())
+print(count_increase())
+print(triple_measure())
