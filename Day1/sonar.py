b968e987310a92da6efdf4e1ff44eb8eae4a26ce
ultragorira <lorisdeluca1988@gmail.com>
2021-12-05 10:24:08 +0200

    Refact

--- Day1/sonar.py
+++ Day1/sonar.py
@@ -1,18 +1,14 @@
-stack = []
+x = []
 
 with open(r'./Day1/sonar.txt', 'r') as f:
     content = f.readlines()
-    stack.append(content[0])
+    x = content[0]
 
 cnt = 0
 for txt in content[1:]:
-    if int(txt) > int(stack[0]):
+    if int(txt) > x:
         cnt += 1
-        stack.pop()
-        stack.append(txt)
-    else:
-        stack.pop()
-        stack.append(txt)
+    x = txt
 
 print(cnt)
 
